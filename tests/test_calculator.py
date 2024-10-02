14c4b044811d95d4bd50a87ae920f777be0957ce
Yusra Amir <yusraamir6@gmail.com>
2024-10-02 09:46:47 -0400

    test case update

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,17 +4,17 @@ sys.path.append("/root/Homework3/")
 from calculator import Calculator
 def test_addition():
     '''Test that addition function works '''    
-    assert Calculator.add(2,2) == 4
+    assert Calculator.add(3,2) == 5
 
 def test_subtraction():
     '''Test that subtraction function works '''    
-    assert Calculator.subtract(2,2) == 0
+    assert Calculator.subtract(3,2) == 1
 
 def test_multiply():
     '''Test that multiplication function works '''    
-    assert Calculator.multiply(2,2) == 4
+    assert Calculator.multiply(3,2) == 6
 
 def test_divide():
     '''Test that division function works '''    
-    assert Calculator.divide(2,2) == 1
+    assert Calculator.divide(4,2) == 2
        
