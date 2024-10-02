e20674f87e56bc6c02afe283ab6d8ec3b81f4057
Yusra Amir <yusraamir6@gmail.com>
2024-10-02 06:28:17 -0400

    created classes

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,5 +1,23 @@
-def add(a,b):
-    return a + b
+import sys
+sys.path.append("/root/Homework3/")
 
-def subtract(a,b):
-    return a - b
+from calculator.operations import add, subtract, multiply, divide
+from calculator.calculation import Calculation
+
+class Calculator:
+    @staticmethod
+    def add(a,b):
+        calculation = Calculation(a,b,add)
+        return calculation.get_result()
+    @staticmethod
+    def subtract(a,b):
+        calculation = Calculation(a,b,subtract)
+        return calculation.get_result()
+    @staticmethod
+    def multiply (a,b):
+        calculation = Calculation(a,b,multiply)
+        return calculation.get_result()
+    @staticmethod
+    def divide (a,b):
+        calculation = Calculation(a,b,divide)
+        return calculation.get_result()
