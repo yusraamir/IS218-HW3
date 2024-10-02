7560c70848c2f2a8e9cba09994e2f21c359bd19a
Yusra Amir <yusraamir6@gmail.com>
2024-10-02 09:38:07 -0400

    added factory method to class

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,23 +1,28 @@
 import sys
 sys.path.append("/root/Homework3/")
 
+from calculator.calculations import Calculations
 from calculator.operations import add, subtract, multiply, divide
 from calculator.calculation import Calculation
+from decimal import Decimal
+from typing import Callable
 
 class Calculator:
     @staticmethod
-    def add(a,b):
-        calculation = Calculation(a,b,add)
-        return calculation.get_result()
+    def _perform_operation(a:Decimal, b:Decimal, operation: Callable[[Decimal, Decimal],Decimal]):
+        calculation = Calculation (a,b,operation)
+        Calculations.add_calculation(calculation)
+        return calculation.perform()
+    
     @staticmethod
-    def subtract(a,b):
-        calculation = Calculation(a,b,subtract)
-        return calculation.get_result()
+    def add(a:Decimal,b:Decimal) -> Decimal:
+        return Calculator._perform_operation(a,b,add)
     @staticmethod
-    def multiply (a,b):
-        calculation = Calculation(a,b,multiply)
-        return calculation.get_result()
+    def subtract(a:Decimal,b:Decimal) -> Decimal:
+        return Calculator._perform_operation(a,b,subtract)
     @staticmethod
-    def divide (a,b):
-        calculation = Calculation(a,b,divide)
-        return calculation.get_result()
+    def multiply(a:Decimal,b:Decimal) -> Decimal:
+        return Calculator._perform_operation(a,b,multiply)
+    @staticmethod
+    def divide(a:Decimal,b:Decimal) -> Decimal:
+        return Calculator._perform_operation(a,b,divide)
--- calculator/calculation.py
+++ calculator/calculation.py
@@ -1,7 +1,20 @@
+from decimal import Decimal
+from typing import Callable
+from calculator.operations import add, subtract, multiply, divide
+
 class Calculation:
     def __init__(self, a, b, operation):
-        self.a= a
-        self.b= b
-        self.operation= operation
+        self.a = a
+        self.b = b
+        self.operation = operation
+    @staticmethod
+    def create(a:Decimal, b:Decimal, operation:Callable[[Decimal,Decimal],Decimal]):
+        return Calculation(a,b,operation)
+    
+    def perform(self) -> Decimal:
+        return self.operation(self.a,self.b)
+    
+    def __repr__(self):
+        return f"Calculation({self.a},{self.b},{self.operation.__name__})"
     def get_result(self):
         return self.operation(self.a, self.b)
